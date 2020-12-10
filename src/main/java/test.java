c022fd38535c6536dc88d8b1d8e46f599967a382
hp <1301576020@qq.com>
2020-12-10 20:45:06 +0800

    主干操作结束提交

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Git项目测试test");
         System.out.println("dev分支操作");
+        System.out.println("主干操作结束提交");
     }
 }
